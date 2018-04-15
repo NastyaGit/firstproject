2cce198fe3d87086bcfce546cfb2e11faf6d6a9c
Nastya <nastyaisakova60@gmail.com>
2018-04-15 12:41:02 +0300

    "some commit"

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main (void)
 {
 int a,b;
 
-printf("Input 2 whole numbers");
+printf(" Input 2 whole  numbers");
 scanf("%d%d",&a,&b);
-printf("a + b = %d + %d = ",a,b, a+b);
+printf("a + b = %d + %d = ",a, b, a+b);
 }
